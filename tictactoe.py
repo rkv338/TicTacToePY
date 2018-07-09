355a79f7bf98a9620bff185a901e40924972b03f
Rithvik <vikturi99@gmail.com>
2018-07-09 15:08:50 -0500

    Board method that uses a list to create a recognizable board in output

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,6 @@
+from IPython.display import clear_output
 
+def display_board(board):
+	print(board[7] + '|' + board[8] + '|' + board[9])
+	print(board[4] + '|' + board[5] + '|' + board[6])
+	print(board[1] + '|' + board[2] + '|' + board[3])
