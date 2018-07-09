9662d1060023f52d97b78007f4a27d176d154919
Rithvik <vikturi99@gmail.com>
2018-07-09 15:21:00 -0500

    Select a marker ( X or O) to use on the board

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,18 @@
 from IPython.display import clear_output
 
+
 def display_board(board):
 	print(board[7] + '|' + board[8] + '|' + board[9])
 	print(board[4] + '|' + board[5] + '|' + board[6])
 	print(board[1] + '|' + board[2] + '|' + board[3])
+
+def player_input():
+	player1 = ''
+	player2 = ''
+	while player1 != 'X' and player1 != 'O':
+		player1 = input('Player 1, type in X or O to select your marker: \n')
+
+	if player1 == 'X':
+		player2 = 'O'
+	else:
+		player2 = 'X'
